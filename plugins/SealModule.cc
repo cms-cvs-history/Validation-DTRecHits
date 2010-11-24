034150f4cf4f113f28b2fe0ae7b75b78ccb16b46
cmsbuild <>
2010-11-24 18:56:23 +0000

    This commit was manufactured by cvs2git to create tag 'Validation-DTRecHits- CMSSW_3_11_0_pre1'.
    Sprout from master 2008-12-03 12:19:10 UTC Stefano Lacaprara <sha1-1aa582c8b612f9ee45a48bebe61f39053371bafc@cern.ch> 'update for 300'
    Cherrypick from master 2010-11-24 18:56:14 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Make BuildFile.xml consistent with current BuildFile tag':
        BuildFile.xml
        plugins/BuildFile.xml
    Cherrypick from master 2010-04-19 21:14:44 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Temporary backout to get minimal tag':
        plugins/SealModule.cc
    Delete:
        BuildFile
        plugins/BuildFile
        test/DTRecHitQuality.cfg

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -1,4 +1,3 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "Validation/DTRecHits/plugins/DTRecHitQuality.h"
@@ -7,8 +6,7 @@
 #include "Validation/DTRecHits/plugins/DTSegment4DQuality.h"
 
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(DTRecHitQuality);
-DEFINE_ANOTHER_FWK_MODULE(DTSegment2DQuality);
-DEFINE_ANOTHER_FWK_MODULE(DTSegment2DSLPhiQuality);
-DEFINE_ANOTHER_FWK_MODULE(DTSegment4DQuality);
+DEFINE_FWK_MODULE(DTRecHitQuality);
+DEFINE_FWK_MODULE(DTSegment2DQuality);
+DEFINE_FWK_MODULE(DTSegment2DSLPhiQuality);
+DEFINE_FWK_MODULE(DTSegment4DQuality);
